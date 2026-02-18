98db098d10dc0e7d4165c6df1f7d372fbefa43af
Michael Ferguson <michaef@umich.edu>
2026-02-18 17:33:05 -0500

    push

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def download_detailweek(week):
         url = f"https://media.githubusercontent.com/media/michaeff/Bleo-website/main/static/czi_images_detailed/week{week}/week{week}.czi"
         return redirect(url)
     elif week0=="o":
-        url = f"https://media.githubusercontent.com/media/michaeff/Bleo-website/main/static/overview/{week[8:]}/overview.czi"
+        url = f"https://media.githubusercontent.com/media/michaeff/Bleo-website/main/static/overview/{week[8:]}/over.czi"
         return redirect(url)
     else:
         weekrest= week[1:]
@@ -91,7 +91,7 @@ def download_detailweektif(week):
         url = f"https://media.githubusercontent.com/media/michaeff/Bleo-website/main/static/czi_images_detailed/week{week}/week{week}.tif"
         return redirect(url)
     elif week0=="o":
-        url = f"https://media.githubusercontent.com/media/michaeff/Bleo-website/main/static/overview/{week[8:]}/overview.tif"
+        url = f"https://media.githubusercontent.com/media/michaeff/Bleo-website/main/static/overview/{week[8:]}/over.tif"
         return redirect(url)
     else:
         weekrest= week[1:]
